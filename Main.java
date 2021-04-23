f43750deb7839e1bf512c92c61d834a17f52ef20
Bandipo Taiye Olusayo <>
2021-04-23 08:43:17 +0000

    Regex code modified

--- Main.java
+++ Main.java
@@ -11,5 +11,22 @@ class Main {
     System.out.println("Using the wildcard character '.' ");
     System.out.println(string.replaceAll(".am", "are"));
     System.out.println(string.replaceAll(".", "*"));
+
+
+    //Using The Caret character '^'
+    System.out.println();
+    System.out.println("Using the Caret");
+    System.out.println(string.replaceAll("^I am"," You are "));
+// to match from beginnning to the end use Caret and Dollar sign '$''
+
+    System.out.println(string.replaceAll("^I am$","You are"));
+
+    //use String.matches(regex) to check if the String contains a regex
+    
+    System.out.println(string.matches("I am"));
+
+    // Using Square bracket
+
+
   }
 }
