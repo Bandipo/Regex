2f85d093d5c450e5a665beac734bc5ab48c94bd5
Bandipo Taiye Olusayo <>
2021-04-23 13:48:13 +0000

    modifications made

--- Main.java
+++ Main.java
@@ -7,29 +7,28 @@ class Main {
     String newString1 = newString.replaceAll("am", "are");
     System.out.println(newString1);
 
-    //Using the wildcard character '.'
+    // Using the wildcard character '.'
     System.out.println("Using the wildcard character '.' ");
     System.out.println(string.replaceAll(".am", "are"));
     System.out.println(string.replaceAll(".", "*"));
 
-
-    //Using The Caret character '^'
+    // Using The Caret character '^'
     System.out.println();
     System.out.println("Using the Caret");
-    System.out.println(string.replaceAll("^I am"," You are "));
-// to match from beginnning to the end use Caret and Dollar sign '$''
+    System.out.println(string.replaceAll("^I am", " You are "));
+    // to match from beginnning to the end use Caret and Dollar sign '$''
 
-    System.out.println(string.replaceAll("^I am$","You are"));
+    System.out.println(string.replaceAll("^I am$", "You are"));
 
-    //use String.matches(regex) to check if the String contains a regex
-    
+    // use String.matches(regex) to check if the String contains a regex
+
     System.out.println(string.matches("I am"));
 
     // Using Square bracket
-     System.out.println("-".repeat(40));
+    System.out.println("-".repeat(40));
     System.out.println(newString1);
-// all occurences of 'a' is replaced with '*'
-    System.out.println(newString1.replaceAll("[a]","*"));
+    // all occurences of 'a' is replaced with '*'
+    System.out.println(newString1.replaceAll("[a]", "*"));
     // all occurences of 'a' followed by 'r' . dont add space between [a] and [r]
     System.out.println(newString1.replaceAll("[a][r]", "*"));
 
@@ -41,13 +40,13 @@ class Main {
 
     //
 
-    //replace every occurences of 'e' in the string with '*'
+    // replace every occurences of 'e' in the string with '*'
     System.out.println(sentence.replaceAll("[e]", "*"));
     // Making your own character classes
 
     String alphanumeric = "abcdefg123456ABCDEFG8910";
     System.out.println(alphanumeric);
-    System.out.println(alphanumeric.replaceAll("[abcABC123]","*"));
+    System.out.println(alphanumeric.replaceAll("[abcABC123]", "*"));
     // or you could do
 
     System.out.println(alphanumeric.replaceAll("[a-cA-B1-3]", "+"));
@@ -57,12 +56,21 @@ class Main {
     System.out.println(alphanumeric.replaceAll("(?i)[a-c]", "*"));
 
     System.out.println("-".repeat(40));
-     //Repalce all digits in alphanumeric with '*' using \\d
-    System.out.println("Replacing all digits in "+alphanumeric);
-    System.out.println(alphanumeric.replaceAll("\\d","*"));
-    //replace all non Digits
-    System.out.println("Replacing all non-digits in "+alphanumeric);
-    System.out.println(alphanumeric.replaceAll("\\D","*"));
+    // Repalce all digits in alphanumeric with '*' using \\d
+    System.out.println("Replacing all digits in " + alphanumeric);
+    System.out.println(alphanumeric.replaceAll("\\d", "*"));
+    // replace all non Digits
+    System.out.println("Replacing all non-digits in " + alphanumeric);
+    System.out.println(alphanumeric.replaceAll("\\D", "*"));
+    System.out.println("-".repeat(40));
+    // replacing all space
+    String whiteSpace = "I \t love \tjava";
+    System.out.println(whiteSpace);
+   
+    System.out.println(whiteSpace.replaceAll("\\s",""));
 
+    System.out.println("replacing All none White space");
+
+     System.out.println(whiteSpace.replaceAll("\\S","*"));
   }
 }
